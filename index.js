1187aba8e7674c4b588e75c85946f830af8008d1
Tommy Tran <tommyktran@gmail.com>
2020-11-20 08:00:57 -0500

    Add state machine

--- index.js
+++ index.js
@@ -52,47 +52,93 @@ function convertCSVtoVotes(csvFile) {
 
     let voteArray = readCSV(csvFile);
     for (x in voteArray) {
-        if (!(voteArray[x].includes('"'))) {
-            voteArray[x] = voteArray[x].split(",");
-        } else {
-            let foundEndQuote = true;
-            voteArray[x] = voteArray[x].split('');
-            for (let i = 0; i < voteArray[x].length; i++) {
-                if (voteArray[x][i] == '"' && foundEndQuote == true) {
-                    foundEndQuote = false;
-                } else if (voteArray[x][i] == '"' && voteArray[x][i+1] == '"') {
-                    i++;
-                } else if (voteArray[x][i] == "," && foundEndQuote == false) {
-                    voteArray[x][i] = "@";
-                } else if (voteArray[x][i] == '"' && foundEndQuote == false) {
-                    foundEndQuote = true;
-                } 
-            }
-            voteArray[x] = voteArray[x].join('');
-            voteArray[x] = voteArray[x].split(',');
-            console.log(voteArray[x]);
-            for (y in voteArray[x]) {
-                if (voteArray[x][y].includes("\r")) {
-                    voteArray[x][y] = voteArray[x][y].replace(/\r\n|\n|\r/, '');
+        voteArray[x] = voteArray[x].split("");
+        let storedArray = [];
+        let storedWord = "";
+        let state = "START";
+        for (let y = 0; y < voteArray[x].length; y++) {
+            if (state == "START") {
+                if (voteArray[x][y] == '"') {
+                    state = "INSIDE_QUOTE";
+                } else if (voteArray[x][y] == ",") {
+                    storedArray.push("");
+                } else {
+                    storedWord += voteArray[x][y];
+                    state = "INSIDE_UNQUOTE";
                 }
-                if (voteArray[x][y][0] == '"') {
-                    console.log(voteArray[x][y])
-                    voteArray[x][y] = voteArray[x][y].split('');
-                    voteArray[x][y].shift();
-                    voteArray[x][y].pop();
-                    voteArray[x][y] = voteArray[x][y].join('');
+            } else if (state == "INSIDE_UNQUOTE") {
+                if (voteArray[x][y] == ",") {
+                    storedArray.push(storedWord);
+                    storedWord = "";
+                    state = "START";
+                } else if (y == voteArray[x].length-1) {
+                    storedWord += voteArray[x][y];
+                    storedArray.push(storedWord);
+                    storedWord = "";
+                    state = "START";
+                } else {
+                    storedWord += voteArray[x][y];
                 }
-                while (voteArray[x][y].indexOf('@') !== -1) {
-                    voteArray[x][y] = voteArray[x][y].replace("@",",");
-                }       
-                while (voteArray[x][y].indexOf('""') !== -1) {
-                    voteArray[x][y] = voteArray[x][y].replace('""','"');
-                }                
+            } else if (state == "INSIDE_QUOTE") {
+                if (voteArray[x][y] == '"' && voteArray[x][parseInt(y)+1] == '"') {
+                    storedWord += '"';
+                    y++;
+                } else if (voteArray[x][y] == '"') {
+                    storedArray.push(storedWord);
+                    storedWord = "";
+                    state = "START";
+                    y++;
+                } else {
+                    storedWord += voteArray[x][y];
+                }
             }
         }
+        voteArray[x] = storedArray;
         voteArray[x].shift();
         voteArray[x].shift();
     }
+    // for (x in voteArray) {
+    //     if (!(voteArray[x].includes('"'))) {
+    //         voteArray[x] = voteArray[x].split(",");
+    //     } else {
+    //         let foundEndQuote = true;
+    //         voteArray[x] = voteArray[x].split('');
+    //         for (let i = 0; i < voteArray[x].length; i++) {
+    //             if (voteArray[x][i] == '"' && foundEndQuote == true) {
+    //                 foundEndQuote = false;
+    //             } else if (voteArray[x][i] == '"' && voteArray[x][i+1] == '"') {
+    //                 i++;
+    //             } else if (voteArray[x][i] == "," && foundEndQuote == false) {
+    //                 voteArray[x][i] = "@";
+    //             } else if (voteArray[x][i] == '"' && foundEndQuote == false) {
+    //                 foundEndQuote = true;
+    //             } 
+    //         }
+    //         voteArray[x] = voteArray[x].join('');
+    //         voteArray[x] = voteArray[x].split(',');
+    //         console.log(voteArray[x]);
+    //         for (y in voteArray[x]) {
+    //             if (voteArray[x][y].includes("\r")) {
+    //                 voteArray[x][y] = voteArray[x][y].replace(/\r\n|\n|\r/, '');
+    //             }
+    //             if (voteArray[x][y][0] == '"') {
+    //                 console.log(voteArray[x][y])
+    //                 voteArray[x][y] = voteArray[x][y].split('');
+    //                 voteArray[x][y].shift();
+    //                 voteArray[x][y].pop();
+    //                 voteArray[x][y] = voteArray[x][y].join('');
+    //             }
+    //             while (voteArray[x][y].indexOf('@') !== -1) {
+    //                 voteArray[x][y] = voteArray[x][y].replace("@",",");
+    //             }       
+    //             while (voteArray[x][y].indexOf('""') !== -1) {
+    //                 voteArray[x][y] = voteArray[x][y].replace('""','"');
+    //             }                
+    //         }
+    //     }
+    //     voteArray[x].shift();
+    //     voteArray[x].shift();
+    // }
     console.log(voteArray);
     return voteArray;
 }
